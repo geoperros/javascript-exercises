4560b012502beca05751ca9426404aeabeec71df
geoperros <geoperros@hotmail.com>
2023-05-12 21:27:52 +0300

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input_ar, ...args) {
+    new_Array = []
+    input_ar.forEach((item) =>{
+        if (!args.includes(item)){
+            new_Array.push(item);
+        }})
+    return new_Array
 };
 
 // Do not edit below this line

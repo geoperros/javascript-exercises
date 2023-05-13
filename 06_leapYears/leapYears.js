7cce79938f7b37eee835dab99f68e9809fba0bce
geoperros <geoperros@hotmail.com>
2023-05-13 23:04:34 +0300

    Finished 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(input_year) {
+    if (input_year % 4 == 0) {
+        if (input_year % 100 == 0 && input_year % 400 ==0){
+            return true;
+        } else if (input_year % 100 == 0){
+            return false;
+        } else {
+            return true;
+        }
+
+
+    }else{
+        return false;
+    }
 
 };
 

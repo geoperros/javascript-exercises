cd81eb97e368f0b6cb611ed7d766cdeb53a1b64d
geoperros <geoperros@hotmail.com>
2023-05-13 18:05:35 +0300

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(val_1,val_2) {
+    let num_1 = 0;
+    let num_2 = 0;
+    let sum_1 = 0;
+    if (Number.isInteger(val_1 && val_2) && val_1 >= 0 && val_2 >= 0){
+        if (val_1 < val_2){
+            num_1 = val_1;
+            num_2 = val_2;
+        } else if(val_1 > val_2){
+            num_1 = val_2;
+            num_2 = val_1;
+        }
+        while (num_1 <= num_2){
+            sum_1 = sum_1 + num_1;
+            num_1++;
+        }
+        return sum_1;
+
+
+    }else{
+        return "ERROR";
+    }
 
 };
 

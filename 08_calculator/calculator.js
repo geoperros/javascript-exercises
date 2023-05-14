a2cc1989cdf4ad9d6bf1d22ba13b8014a969da9b
geoperros <geoperros@hotmail.com>
2023-05-14 19:36:51 +0300

    Finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(x,y) {
+  return (x + y);
 	
 };
 
-const subtract = function() {
+const subtract = function(x,y) {
+  return (x - y);
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let i = 0;
+  let result = 0;
+  while (i < arr.length){
+    result = result + arr[i]
+    i++
+  }
+  return result;
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let i = 1;
+  let result = arr[0];
+  while (i < arr.length){
+    result = result * arr[i]
+    i++
+  }
+  return result;
 
 };
 
-const power = function() {
+const power = function(x,y) {
+  return (x ** y);
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line

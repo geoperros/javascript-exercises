bc5ca4609d9171e4bd5ffd7baae45760f167ed2f
geoperros <geoperros@hotmail.com>
2023-05-15 22:43:08 +0300

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let lower_p = phrase.toLowerCase();
+    let arr_3 = [];
+    let arr = lower_p.split("");
+    let arr_2 = [];
+    for ( let i = 0; arr.length > i; i++){
+        if (arr[i] != "!" && arr[i] != " " && arr[i] != "," && arr[i] != "."){
+            arr_3.push(arr[i])
+        }
+    }
+    for (let i = arr_3.length; i > 0;){
+        arr_2.push(arr_3[(i - 1)]);
+        i--;
+    }
+    for (let i = 0; arr_3.length > i;){
+        if (arr_3[i] != arr_2[i]){
+            return false;
+        }
+        i++
+    }
+    return true;
+
 
 };
 

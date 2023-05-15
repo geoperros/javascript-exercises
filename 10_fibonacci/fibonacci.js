167435573fabf490c0bc1ea8ce23ae7ff1749458
geoperros <geoperros@hotmail.com>
2023-05-15 23:02:57 +0300

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num_int =Number(num);
+    if (num_int < 0){
+        return "OOPS"
+    }
+    let fibo = [0,1]
+    for (let i = 2; i - 1 < num_int;){
+        if (num_int == 0){
+            return 0
+        }else if(num_int == 1){
+            return 1
+        }else{
+            fibo.push(fibo[i-1]+fibo[i-2])
+        }
+        i++
+    }
+    return fibo[num_int]
 
 };
 
